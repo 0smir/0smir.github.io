b1646a4576ddfc26b4b9ba5ba39c1f4f97f0d803
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-06-18 19:45:30 +0300

    task#: add different  files for script send form

--- poland-bootstrap/js/sendform.js
+++ poland-bootstrap/js/sendform.js
@@ -11,27 +11,27 @@ $(document).ready(function(){
 		});
 	});
 	
-	$('#modalForm').submit(function(e){
-		e.preventDefault();
-		$.ajax({
-			url:'https://formspree.io/o.v.smirnova1987@gmail.com',
-			type:'post',
-			data:$('#myForm').serialize(),
-			success:function(){
-			}
-		});
-	});
+	// $('#modalForm').submit(function(e){
+	// 	e.preventDefault();
+	// 	$.ajax({
+	// 		url:'https://formspree.io/o.v.smirnova1987@gmail.com',
+	// 		type:'post',
+	// 		data:$('#myForm').serialize(),
+	// 		success:function(){
+	// 		}
+	// 	});
+	// });
 
-	$('#myBottomForm').submit(function(e){
-		e.preventDefault();
-		$.ajax({
-			url:'https://formspree.io/o.v.smirnova1987@gmail.com',
-			type:'post',
-			data:$('#myForm').serialize(),
-			success:function(){
-			}
-		});
-	});
+	// $('#myBottomForm').submit(function(e){
+	// 	e.preventDefault();
+	// 	$.ajax({
+	// 		url:'https://formspree.io/o.v.smirnova1987@gmail.com',
+	// 		type:'post',
+	// 		data:$('#myForm').serialize(),
+	// 		success:function(){
+	// 		}
+	// 	});
+	// });
 
 
 
