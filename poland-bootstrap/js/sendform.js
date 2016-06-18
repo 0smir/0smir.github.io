5a631f401a2052f40dcb9ce4409e80acb278ecb6
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-06-18 18:13:59 +0300

    Task#: add different scripts for all forms

--- poland-bootstrap/js/sendform.js
+++ poland-bootstrap/js/sendform.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	$('#myForm, #modalForm, myBottomForm').submit(function(e){
+	$('#myForm').submit(function(e){
 		e.preventDefault();
 		$.ajax({
 			url:'https://formspree.io/o.v.smirnova1987@gmail.com',
@@ -10,6 +10,29 @@ $(document).ready(function(){
 			}
 		});
 	});
+
+	$('#modalForm').submit(function(e){
+		e.preventDefault();
+		$.ajax({
+			url:'https://formspree.io/o.v.smirnova1987@gmail.com',
+			type:'post',
+			data:$('#myForm').serialize(),
+			success:function(){
+			}
+		});
+	});
+
+	$('#modalForm').submit(function(e){
+		e.preventDefault();
+		$.ajax({
+			url:'https://formspree.io/o.v.smirnova1987@gmail.com',
+			type:'post',
+			data:$('#myForm').serialize(),
+			success:function(){
+			}
+		});
+	});
+
 
 
 });
