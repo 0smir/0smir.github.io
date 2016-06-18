97340029f9aed44031568b5c33796079869e3119
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-06-18 19:49:35 +0300

    change mail for form#3

--- poland-bootstrap/js/sendform3.js
+++ poland-bootstrap/js/sendform3.js
@@ -1,11 +1,8 @@
 $(document).ready(function(){
-
-	
-
 	$('#myBottomForm').submit(function(e){
 		e.preventDefault();
 		$.ajax({
-			url:'https://formspree.io/o.v.smirnova1987@gmail.com',
+			url:'https://formspree.io/pupsua@gmail.com',
 			type:'post',
 			data:$('#myForm').serialize(),
 			success:function(){
