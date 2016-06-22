726b1051d180ad6b26f31ffb9a808ae8fea67a90
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-06-22 18:59:57 +0300

    task#: test succsess text

--- poland-bootstrap/js/sendform.js
+++ poland-bootstrap/js/sendform.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
 
-	$('#myForm').submit(function(e){
-		e.preventDefault();
+	$('#myForm').submit(function(event){
+		event.preventDefault();
+
 		$.ajax({
 			url:'https://formspree.io/o.v.smirnova1987@gmail.com',
 			type:'post',
 			data:$('#myForm').serialize(),
-			success:function(){
+			success:function(data, status, jqXHR){
 				$('.result-form').find('span').addClass('succsess');	
+			},
+			error:function(data, status, jqXHR){
+				
 			}
 		});
+
 	});
 
 
